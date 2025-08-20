5b0ff30ea901179ea565d2370e1f2a6ccf3bedd6
ingJuanArenas <juanarenasing@gmail.com>
2025-08-20 10:18:53 -0500

    initial commit

---
+++ src/test/java/com/tasks/management/tasks_management/TasksManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks.management.tasks_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
